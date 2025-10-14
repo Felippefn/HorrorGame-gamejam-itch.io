bdb4daaaa5aaafadfce44b9bb2efe5604337da5c
Felippe Nagy <felippenagy@outlook.com>
2025-10-13 21:51:52 -0300

    feat: :rocket: added scenario and task scripts for tv and radio

---
+++ Assets/Scripts/LookAtObjects.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LookAtObjects : MonoBehaviour
+{
+
+    public Text textObject;
+    public string description = "Description";
+
+    public bool inReach;
+
+
+    void Start()
+    {
+        textObject.GetComponent<Text>().enabled = false;        
+    }
+
+    void OTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            textObject.GetComponent<Text>().enabled = true;
+            //textObject.text = description;
+            inReach = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Reach")
+        {
+            textObject.GetComponent<Text>().enabled = false;
+            inReach = false;
+            textObject.GetComponent<Text>().text = "";
+        }
+    }
+
+    void Update()
+    {
+        if(inReach && Input.GetButtonDown("Interact"))
+        {
+            textObject.text = description.ToString();
+        }
+    }
+}
